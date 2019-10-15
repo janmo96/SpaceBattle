ebb182fb88c60f7321f3c5bc7d99b88a81424db7
janmo96 <janmo96@gmail.com>
2019-10-15 21:27:15 +0300

    First Push
    First Push

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+
+    public float h;
+    public float v;
+
+    public float maxXPos;
+    public float minXPos;
+
+    public float smooth = 5.0F;
+    public float tiltAngle = 30.0F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, minXPos, maxXPos), transform.position.y);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+
+        if(h < 0)
+        {
+            Quaternion target = Quaternion.Euler(new Vector3(0, 0, 30));
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+            rb.AddForce(new Vector2(-speed, 0) * Time.deltaTime, ForceMode2D.Force);
+        } else if (h > 0)
+        {
+            Quaternion target = Quaternion.Euler(new Vector3(0,0, -30));
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+            rb.AddForce(new Vector2(speed, 0) * Time.deltaTime, ForceMode2D.Force);
+        } else if (h == 0)
+        {
+            Quaternion target = Quaternion.Euler(new Vector3(0, 0, 0));
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Assets/ScrollingBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+
+    public float speed; //scroll speed for background
+    public float clamppos; // clamping position
+    public Vector3 StartingPositioon; // get background start pos
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartingPositioon = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float newPos = Mathf.Repeat(Time.time * speed, clamppos);
+        transform.position = StartingPositioon + -Vector3.up * newPos;
+    }
+}
